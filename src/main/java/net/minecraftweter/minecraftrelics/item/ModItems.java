118c9b319551b1b57c96d1023cfdbebffce9c11a
Jan Claes <janclaes12@gmail.com>
2026-08-12 10:36:54 +0200

    add wind feather relic item + bugfix from switching branches

--- src/main/java/net/minecraftweter/minecraftrelics/item/ModItems.java
+++ src/main/java/net/minecraftweter/minecraftrelics/item/ModItems.java
@@ -1,11 +1,22 @@
 package net.minecraftweter.minecraftrelics.item;
 
+import net.minecraft.world.item.Item;
 import net.minecraftweter.minecraftrelics.MinecraftRelics;
+import net.minecraftweter.minecraftrelics.item.relic.RelicCategory;
+import net.minecraftweter.minecraftrelics.item.relic.RelicItem;
+import net.minecraftweter.minecraftrelics.item.relic.RelicRarity;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MinecraftRelics.MOD_ID);
+
+    public static final DeferredItem<Item> WIND_FEATHER_RELIC = ITEMS.register(
+            "wind_feather_relic", () -> new RelicItem(
+                    "wind_feather_relic", RelicRarity.UNCOMMON, RelicCategory.DEFENSE
+            )
+    );
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
