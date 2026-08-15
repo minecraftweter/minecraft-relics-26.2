7829873e1b9c1c4f38d051d055a5b3c09a01c458
Jan Claes <janclaes12@gmail.com>
2026-08-15 14:47:50 +0200

    Add keybind for relic inventory

--- src/main/java/net/minecraftweter/minecraftrelics/MinecraftRelicsClient.java
+++ src/main/java/net/minecraftweter/minecraftrelics/MinecraftRelicsClient.java
@@ -1,12 +1,17 @@
 package net.minecraftweter.minecraftrelics;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftweter.minecraftrelics.dataAttachment.ModDataAttachments;
+import net.minecraftweter.minecraftrelics.keymapping.ModKeyMappings;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
