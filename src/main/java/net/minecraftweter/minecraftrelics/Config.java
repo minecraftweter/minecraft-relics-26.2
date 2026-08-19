cab265f8467404f556ebe1aa8dda7c9a265e0c2d
Jan Claes <janclaes12@gmail.com>
2026-08-19 18:08:35 +0200

    remove redundant newline in Config.java

--- src/main/java/net/minecraftweter/minecraftrelics/Config.java
+++ src/main/java/net/minecraftweter/minecraftrelics/Config.java
@@ -4,6 +4,5 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public final class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-
     static final ModConfigSpec SPEC = BUILDER.build();
 }
