fcf2c3e911a9c3e593bbcc672f6a5f9eef964e2b
Jan Claes <janclaes12@gmail.com>
2026-08-21 10:30:49 +0200

    fix relic inventory save data

--- src/main/java/net/minecraftweter/minecraftrelics/player/RelicInventory.java
+++ src/main/java/net/minecraftweter/minecraftrelics/player/RelicInventory.java
@@ -1,23 +1,32 @@
 package net.minecraftweter.minecraftrelics.player;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.gamerules.GameRules;
 import net.minecraftweter.minecraftrelics.MinecraftRelics;
+import net.minecraftweter.minecraftrelics.data.ModDataAttachments;
 import net.minecraftweter.minecraftrelics.gui.custom.RelicInventoryMenu;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.transfer.item.ItemStacksResourceHandler;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+@EventBusSubscriber(modid = MinecraftRelics.MOD_ID)
 public class RelicInventory extends ItemStacksResourceHandler implements MenuProvider {
     public static final int SIZE = 6;
     public RelicInventory() {
         super(SIZE);
     }
 
-    /* MENU */
+    /* Menu */
 
     @Override
     public @NonNull Component getDisplayName() {
@@ -28,5 +37,26 @@ public class RelicInventory extends ItemStacksResourceHandler implements MenuPro
     public @Nullable AbstractContainerMenu createMenu(int i, @NonNull Inventory inventory, @NonNull Player player) {
         return new RelicInventoryMenu(i, inventory);
     }
+
+    @SubscribeEvent
+    public static void playerDeathEvent(LivingDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        if(entity instanceof Player player && !((ServerLevel) entity.level()).getGameRules().get(GameRules.KEEP_INVENTORY)) {
+            RelicInventory relicInventory = player.getData(ModDataAttachments.RELIC_INVENTORY);
+            relicInventory.copyToList().forEach(stack -> player.drop(stack, false));
+            player.setData(ModDataAttachments.RELIC_INVENTORY, new RelicInventory());
+        }
+    }
+
+    @SubscribeEvent
+    public static void playerCloneEvent(PlayerEvent.Clone event) {
+        Player oldPlayer = event.getOriginal();
+        Player newPlayer = event.getEntity();
+        newPlayer.setData(
+                ModDataAttachments.RELIC_INVENTORY,
+                oldPlayer.getData(ModDataAttachments.RELIC_INVENTORY)
+        );
+    }
+
 
 }
